addd084245bd7c6b9e1712559267e6a55bed86fd
Your Name <you@example.com>
2023-07-26 10:39:56 +0200

    0.4.1 input field

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,17 +3,21 @@ package org.example;
 import javax.swing.*;
 
 public class App {
-
-    public static final int LIMIT = 10000;
-
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            String input = JOptionPane.showInputDialog(null, "Введите количество элементов ряда:", "10000");
+            int limit;
+            try {
+                limit = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                limit = 10000; // значение по умолчанию, если пользователь введет некорректное значение или ничего не введет
+            }
+
             HarmonicSeriesView view = new HarmonicSeriesView();
             HarmonicSeriesCalculator model = new HarmonicSeriesCalculator();
             HarmonicSeriesController controller = new HarmonicSeriesController(model, view);
 
-            controller.displaySeries(LIMIT);
+            controller.displaySeries(limit);
         });
     }
-
 }
