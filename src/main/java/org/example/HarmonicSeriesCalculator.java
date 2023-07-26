7a30d2d87a4b897cd31e2adecbfc5e30dc8cc521
Your Name <you@example.com>
2023-07-26 09:46:49 +0200

    0.2.1

--- src/main/java/org/example/HarmonicSeriesCalculator.java
+++ src/main/java/org/example/HarmonicSeriesCalculator.java
@@ -3,9 +3,7 @@ package org.example;
 public class HarmonicSeriesCalculator {
     public double calculateSum(int limit) {
         double sum = 0.0;
-        for (int n = 2; n <= limit + 1; n++) {
-            sum += 1.0 / n;
-        }
+        for (int n = 2; n <= limit + 1; n++) sum += 1.0 / n;
         return sum;
     }
 
