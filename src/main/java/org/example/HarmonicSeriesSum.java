674efc03d3492890a16e42a13c5842e9ff3c21d7
Your Name <you@example.com>
2023-07-26 09:35:24 +0200

    0.1.0

--- src/main/java/org/example/HarmonicSeriesSum.java
+++ src/main/java/org/example/HarmonicSeriesSum.java
@@ -10,22 +10,16 @@ public class HarmonicSeriesSum {
         int n = 2;  // начальное значение (1 уже не учитываем)
         int limit = 2_140_000;  // предел для количества членов ряда
 
-        System.out.println("  Член ряда            Сумма");
+        System.out.println("  Term                Sum");
         System.out.println("---------            -----");
 
         for (; n <= limit + 1; n++) {
             sum += 1.0 / n;
 
-            if (n % 1000 == 0) {
-                System.out.printf("%10d    %.15f%n", n, sum);
-            }
+            if (n % 1000 == 0) System.out.printf("%10d    %.15f%n", n, sum);
         }
 
-        System.setOut(new PrintStream(System.out, true, "UTF-8"));
-
         System.out.printf("\nFinal series sum after  %d terms: %.15f%n", limit, sum);
-
-
         System.out.print("Integer.MAX_VALUE = " + Integer.MAX_VALUE);
     }
 
